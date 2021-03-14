f3f5b803c40a26af03c5a9ff893c2d8ff320227d
handleman <pavel.handleman@gmail.com>
2021-03-14 14:55:40 +0300

    styling config page

--- pages/config.jsx
+++ pages/config.jsx
@@ -1,8 +1,56 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
-        <div><h1>config</h1></div>
+        <div>
+            <form className="config-panel">
+                <h2>Please adjust timer and rest intervals </h2>
+                <h3>(in minutes)</h3>
+                <label htmlFor="interval">
+                    Timer:
+                </label>
+                <input type="number" name="interval" id="interval" />
+                <label htmlFor="rest">
+                    Rest:
+                </label>
+                <input type="number" name="rest" id="rest" />
+
+                </form>
+                <style jsx lang="scss">{`
+                    $darkgray: #777;
+                    .config-panel{
+                
+                        max-width: 350px;
+                        width: 100%;
+                        display: flex;
+                        flex-direction: column;
+                        margin-right: auto;
+                        margin-left: auto;
+                    }
+                    input{
+                        appearance: none;
+                        display: inline-block;
+                        min-height: 36px;
+                        padding: 8px 12px;
+                        width: 100%;
+                        margin-top: 5px;
+                        background: #fff;
+                        border: 1px solid #d9d9d9;
+                        border-top: 1px solid #c0c0c0;
+                        box-sizing: border-box;
+                        border-radius: 1px;
+                        font-size: 15px;
+                        color: $darkgray;
+                        outline: none;
+                        margin-bottom: 1em;
+                    }
+                    label{
+                        font-size: 1em;
+                        margin-bottom: 0.5em;
+                        color: $darkgray;
+                    }
+                `}
+                </style>
+        </div>
     )
 }
