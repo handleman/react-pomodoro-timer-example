108bded1af4023a85590d9399c7da81cf50bffad
Pavel Hendelman <pavel.handleman@gmail.com>
2021-03-24 10:45:16 +0300

    countdown state slice

---
+++ store/countDownSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { countDownDefaults } from './defaults';
+
+export const countDownSlice = createSlice({
+    name: 'countDown',
+    initialState: countDownDefaults,
+    reducers: {
+        setStatus: (state, action) => {
+            state.status = action.payload;
+        },
+        setMinutes: (state, action) => {
+            state.minutes = action.payload
+        },
+        setSeconds: (state, action) => {
+            state.seconds = action.payload
+        },
+        setInterval: (state, action) => {
+            state.interval = action.payload
+        },
+        setRest: (state, action) => {
+            state.rest = action.payload
+        },
+        setRestMode: (state, action) => {
+            state.restMode = action.payload
+        },
+
+    }
+
+});
+
+export const {
+    setStatus,
+    setMinutes,
+    setSeconds,
+    setInterval,
+    setRest,
+    setRestMode,
+} = countDownSlice.actions;
+
+export default countDownSlice.reducer;
