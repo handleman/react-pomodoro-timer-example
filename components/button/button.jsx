6171a95f01f3d09c347d34c6ec554cd637f6fc57
Pavel Hendelman <pavel.handleman@gmail.com>
2021-03-27 13:13:39 +0300

    component connected to actions

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -1,13 +1,13 @@
 import styles from './button.module.scss';
 
 export function Button(props) {
-    const { primary, accent, children: caption } = props;
+    const { primary, accent, children: caption, onClick } = props;
     let className = styles.button;
     primary && (className += ` ${styles.primary}`);
     accent && (className += ` ${styles.accent}`);
 
     return (
-        <button type="button" className={className}>
+        <button type="button" onClick={onClick} className={className}>
             { caption}
         </button>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,17 +3,42 @@ import styles from '../styles/Home.module.css'
 import { Pomodoro } from '../components/pomodoro'
 import { TimerControlsPanel } from '../components/timerControlsPanel'
 import { Button } from '../components/button'
+import { useSelector, useDispatch } from 'react-redux';
+import { selectActiveState, selectPausedState, setActive, setOnPause, reset } from '../store/timerSlice';
 
 // todo: head title
 // todo: move code out to '/features/countdown'
 export default function Index() {
+  const active = useSelector(selectActiveState);
+  const paused = useSelector(selectPausedState);
+  const dispatch = useDispatch();
+  const isTimerReadyForUse = (!active && !paused);
+  const isTimerInUse = (active && !paused);
+  const isTImerOnPause = (active && paused);
+
+  const handleStartTimer = () => {
+    dispatch(setActive());
+  }
+  const handlePauseTimer = () => {
+    dispatch(setOnPause());
+  }
+  const handleResetTimer = () => {
+    dispatch(reset());
+  }
+
   return (
     <div className={styles.timerPanel}>
       <Pomodoro></Pomodoro>
       <TimerControlsPanel>
-        <Button primary>Run</Button>
-        <Button>Pause</Button>
-        <Button accent>Reset</Button>
+        {
+          isTimerReadyForUse && <Button primary onClick={handleStartTimer}>Run</Button>
+        }
+        {
+          isTimerInUse && <Button onClick={handlePauseTimer}>Pause</Button>
+        }
+        {
+          isTImerOnPause && <Button accent onClick={handleResetTimer}>Reset</Button>
+        }
       </TimerControlsPanel>
     </div>
   )
